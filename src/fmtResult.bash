0bc92d3f92e6e8a4b11846f320daa5815420d43c
S.V. Paulauskas <stanpaulauskas@gmail.com>
2013-05-13 21:35:30 -0400

    A nice script to format the fitting results

---
+++ src/fmtResult.bash
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+file=$1
+
+awk '/./' $file > $file.temp
+
+lc=0
+while read line
+do 
+    if (( $lc <= 4 )) 
+    then
+        echo "#" $line >> $file.out
+    else
+        set -- $line
+        if echo $1 | grep -q alpha
+        then
+            pos=${1:5}
+            alpha[$pos]=$2
+            alphaErr[$pos]=$4
+        elif echo $1 | grep -q mu
+        then
+            pos=${1:2}
+            mu[$pos]=$2
+            muErr[$pos]=$4
+        fi
+    fi
+    let lc=$lc+1
+done < $file.temp
+
+rm -f $file.temp
+
+let num=${#alpha[@]}-1
+for i in `seq 0 $num`
+do
+    echo -e "$i  ${mu[$i]} ${muErr[$i]} ${alpha[$i]} ${alphaErr[$i]}" >> $file.out
+done
+
+mv $file.out $file
