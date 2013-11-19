4bed65035156f855aba08e894be503cef33d4ef5
S.V. Paulauskas <stanpaulauskas@gmail.com>
2013-11-19 17:05:39 -0500

    Updating the density scripts

--- scripts/densities.gp
+++ scripts/densities.gp
@@ -3,51 +3,46 @@ load '/home/vincent/.gnuplot'
 set border lw 3
 unset key
 set xlabel "E_x (MeV)"
-set terminal postscript eps enhanced color "NimbusSanL-Regu,18"
+set terminal postscript eps enhanced color solid "NimbusSanL-Regu,18"
 
 sn=4.558
 qbeta=10.490
+res=0.001
 
 set xrange[4:11]
 set x2tics ("S_n" sn, "Q_{/StandardSymL b}" qbeta)
 
-ARSTY="nohead lc rgb comp5 lw 5 lt 5"
+ARSTY="nohead lc rgb secB1 lw 5 lt 5"
 ARSTY1="nohead lc rgb secA3 lw 5 lt 2"
 ARSTY2="nohead lc rgb secB3 lw 5 lt 7"
 
+SENS="'../results/vast/working/working.sens' u 1:($4*res):($5*res) w yerrorbars lc rgb secB1 ps 1.5 lw 2"
+
 #---------- PLOT THE NEUTRON INDIVIDUAL STUFF --------
 file0='../results/vast/working/working.dat'
-set ylabel offset 1,0 "B(GT)"
-set arrow 1 from sn,0 to sn,0.06 @ARSTY
-set arrow 2 from qbeta,0 to qbeta,0.06 @ARSTY1
-set output '../pics/vast/working/working-bgt.eps'
-plot [][0:0.06] file0 u ($3+sn):8 w impulses lw 5 lc rgb prim3,\
-     '../results/vast/working/working.sens' u 1:4:5 w yerrorbars
-
-set ylabel offset 1,0 "Branching Ratio (/100 decays)"
+set ylabel offset 1,0 "I_n (/100 decays)"
 set arrow 1 from sn,0.0002 to sn,0.002 @ARSTY
 set arrow 2 from qbeta,0.0002 to qbeta,0.002 @ARSTY1
 set output '../pics/vast/working/working-br.eps'
 plot file0 u ($3+sn):7 w impulses lw 5 lc rgb prim3
 
-set ylabel offset 1,0 "log(ft)"
-set arrow 1 from sn,4.5 to sn,7 @ARSTY
-set arrow 2 from qbeta,4.5 to qbeta,7 @ARSTY1
-set output '../pics/vast/working/working-logft.eps'
-plot file0 u ($3+sn):10 w impulses lw 5 lc rgb prim3
+set ylabel offset 1,0 "B(GT)"
+set arrow 1 from sn,0 to sn,0.06 @ARSTY
+set arrow 2 from qbeta,0 to qbeta,0.06 @ARSTY1
+set output '../pics/vast/working/working-bgt.eps'
+plot [][0:0.06] file0 u ($3+sn):8 w impulses lw 5 lc rgb prim3, @SENS
+
 
 #---------- PLOT THE NEUTRON DENSITY STUFF --------
 file1='../results/vast/working/working.bgt'
-set ylabel offset 1,0 "Branching Ratio / (0.001 MeV)"
+set ylabel offset 1,0 "I_n / (0.001 MeV)"
 set arrow 1 from sn,0 to sn,0.0004 @ARSTY
 set arrow 2 from qbeta,0 to qbeta,0.0004 @ARSTY1
 set output '../pics/vast/working/working-nden.eps'
-plot [][0:0.0004]file1 u 1:3:4 w filledcurve lc rgb comp5, '' u 1:2 w steps lc rgb prim3 lw 3,\
-     '../results/vast/working/working.sens' u 1:4:5 w yerrorbars
+plot [][0:0.0004]file1 u 1:3:4 w filledcurve lc rgb comp5, '' u 1:2 w steps lc rgb prim3 lw 3
 
 set ylabel offset 1,0 "B(GT) / (0.001 MeV)"
 set arrow 1 from sn,0 to sn,0.00018 @ARSTY
 set arrow 2 from qbeta,0 to qbeta,0.00018 @ARSTY1
 set output '../pics/vast/working/working-nden-bgt.eps'
-plot [4:11][0:0.00018] file1 u 1:6:7 w filledcurve lc rgb comp5, '' u 1:5 w steps lc rgb prim3 lw 3,\
-     '../results/vast/working/working.sens' u 1:4:5 w yerrorbars
+plot [4:11][0:0.00018] file1 u 1:6:7 w filledcurve lc rgb comp5, '' u 1:5 w steps lc rgb prim3 lw 3, @SENS
--- src/sensitivity.cpp
+++ src/sensitivity.cpp
@@ -13,27 +13,45 @@
 
 using namespace std;
 
+int main() {
 //---------- SET SOME DETAILS ABOUT THE EXP HERE ----------
 //---------- HANDLE THIS BETTER LATER ----------
-double numBars = 9;
-Variable omega = Variable(numBars*0.0061,0.0,""); //solid angle from Sergey's sim
-Variable betaEff = Variable(0.23, 0.03, "/100"); // the error needs recalculated.
-pair<Variable,Variable> effs = make_pair(betaEff,omega);
-
-int main() {
-    double res = 0.001;
-
-    //---------- SET THE DECAY INFORMATION HERE ---------
+    double numBars = 9;
+    Variable omega = Variable(numBars*0.0061,0.0,""); 
+    
+    //---------- For the 84Ga decay ---------
+    // Variable betaEff = Variable(0.13, 0.03, "/100"); // 84ga
+    // Variable q(13.690,0.500,"MeV"), sn(5.243,0.004,"MeV"),
+    //     qbn(8.60,0.004,"MeV");
+    // Decay decay(Variable(31,0.0,""),q,sn,qbn, Variable(0.085,0.01, "s")); 
+    // //RawNumGammas, eff_gamma, absBr
+    // decay.SetGammaInfo(Variable(7208,0.0,"counts"),
+    //                    Variable(0.122882,0.00,"/100"),
+    //                    Variable(0.086, 0.0, "/100"));
+    
+    //---------- For the 78cu Decay ---------
+    // Variable betaEff = Variable(0.43, 0.03, "/100"); // 78cu
+    // Variable q(12.990,0.500,"MeV"), sn(6.765,0.003,"MeV"),
+    //     qbn(6.22,0.5,"MeV");
+    // //ParentZ, Q(MeV), Sn(MeV), Qbetan(MeV), T1/2(s)
+    // Decay decay(Variable(29,0.0,""),q,sn,qbn, Variable(0.335,0.011, "s")); 
+    // //RawNumGammas, eff_gamma, absBr
+    // //this info is taken from the 115 keV line associated with the 77Zn->77Ga decay
+    // decay.SetGammaInfo(Variable(13394,0.0,"counts"),
+    //                    Variable(0.214579,0.0122841,"/100"),
+    //                    Variable(0.281, 0.00, "/100")); 
+    
+    //---------- For the 77cu Decay ----------
+    Variable betaEff = Variable(0.23, 0.03, "/100"); // 77cu
     //ParentZ, Q(MeV), Sn(MeV), Qbetan(MeV), T1/2(s)
     Variable q(10.490,0.500,"MeV"), sn(4.5575,0.00025,"MeV"),
         qbn(5.720,0.015,"MeV");
-    Decay decay(Variable(29,0.0,""), q, sn, qbn,
-                Variable(0.4679,0.00021, "s")); 
+    Decay decay(Variable(29,0.0,""),q,sn,qbn,Variable(0.4679,0.00021, "s")); 
     //RawNumGammas, eff_gamma, absBr
     decay.SetGammaInfo(Variable(351222,14751.324,"counts"),
                        Variable(0.0668422,0.003,"/100"),
                        Variable(0.191, 0.006, "/100")); 
-    
+
     ofstream out("results/vast/working/working.sens");
     out << setw(7) << "#Ex(MeV) BR BRerr B(GT) B(GT)err" << endl;
     for(double i = 0.1; i < q.GetValue()-sn.GetValue(); i += 0.1) {
@@ -43,10 +61,10 @@ int main() {
         BGTCalculator bgt(n,decay,betaEff,omega);
 
         out << n.GetExcitationEnergy().GetValue() << " " 
-            << n.GetBranchingRatio().GetValue()*res << " " 
-            << n.GetBranchingRatio().GetError()*res << " " 
-            << n.GetBgt().GetValue()*res << " " 
-            << n.GetBgt().GetError()*res << endl;
+            << n.GetBranchingRatio().GetValue() << " " 
+            << n.GetBranchingRatio().GetError() << " " 
+            << n.GetBgt().GetValue() << " " 
+            << n.GetBgt().GetError() << endl;
     }
     out.close();
 }
