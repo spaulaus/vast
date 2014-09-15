b530517129f2616302518e31647ca4c50a240641
S.V. Paulauskas <stanpaulauskas@gmail.com>
2014-09-15 16:48:33 -0400

    Updating the efficiency curve for the data from Sergey on 09-10-2014

--- src/EffCalculator.cpp
+++ src/EffCalculator.cpp
@@ -48,12 +48,23 @@ Variable EffCalculator::GetSimRollingEff(const Variable &energy) {
 
     //The parameterization of the simulation results from Miguel's 
     //values sent to Sergey on 03-12-2014
-    Variable a = Variable(-3.449922e-10, 1.561986e-10, "");
-    Variable b = Variable(4.477057e-06 ,1.692577e-06,"");
-    Variable c = Variable(-2.302741e-02 ,5.748920e-03,""); 
-    Variable d = Variable(5.416032e+01 ,6.028794e+00,"");
-    double eff = a.GetValue()*pow(en,3) + b.GetValue()*pow(en,2) +
-        c.GetValue()*en+d.GetValue();
+    // Variable a = Variable(-3.449922e-10, 1.561986e-10, "");
+    // Variable b = Variable(4.477057e-06 ,1.692577e-06,"");
+    // Variable c = Variable(-2.302741e-02 ,5.748920e-03,""); 
+    // Variable d = Variable(5.416032e+01 ,6.028794e+00,"");
+    // double eff = a.GetValue()*pow(en,3) + b.GetValue()*pow(en,2) +
+    //     c.GetValue()*en+d.GetValue();
+
+    //This new parameterization is from the data provided to Sergey for 
+    //my 077cu.ban banana number 4, he provided these to me on 09-10-2014 
+    //and the calculation only went through 1.5 MeV, points below that are 
+    //extrapolation.
+    Variable a = Variable(-0.409366, 0.2148, ""); 
+    Variable b = Variable(-2.25262, 1.407, "");
+    Variable c = Variable(24.6331, 2.112, "");
+    double eff = a.GetValue()*pow(en/1000.,2) +
+        b.GetValue()*en/1000.+c.GetValue();
+
     return(Variable(eff/100.,0.0,"/100"));
 }
 
